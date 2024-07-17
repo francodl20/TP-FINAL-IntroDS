46ad712c82b2ea48c391510d6af740f6518c8439
Franco de León <45929292+francodl20@users.noreply.github.com>
2024-07-17 20:12:48 -0300

    Models added

--- Backend/modelos.py
+++ Backend/modelos.py
@@ -3,11 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class DragonType(db.Model):
-    __tablename__ = 'base_dragons'
+class DragonStats(db.Model):
+    __tablename__ = 'dragon_stats'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String[40], nullable = False)
-    attack = db.Column(db.Integer, nullable = False)
+    category = db.Column(db.String[40], nullable = False)
     hp = db.Column(db.Integer, nullable = False)
-    type = db.Column(db.String[40], nullable = False)
+    attack = db.Column(db.Integer, nullable = False)
+    defense = db.Column(db.Integer, nullable = False)
+    max_lvl = db.Column(db.Integer, nullable = False)
+    max_asc = db.Column(db.Integer, nullable = False)
+    
+class GachaChances(db.Model):
+    __tablename__ = 'gacha_chances'
+    id = db.Column(db.Integer, primary_key = True)
+    six_star_percent = db.Column(db.Double, nullable = False)
+    five_star_percent = db.Column(db.Double, nullable = False)
+    four_star_percent = db.Column(db.Double, nullable = False)
+    three_star_percent = db.Column(db.Double, nullable = False)
+
+class GachaPool(db.Model):
+    __tablename__ = 'gacha_pool'
+    id = db.Column(db.Integer, primary_key = True)
+    gacha_id = db.Column(db.Integer, db.ForeignKey('gacha_chances.id'))
+    dragon_id = db.Column(db.Integer, db.ForeignKey('dragon_stats.id'))
     stars = db.Column(db.Integer, nullable = False)
+
+class Players(db.Model):
+    __tablename__ = 'players'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String[40], nullable = False)
+    orbs = db.Column(db.Integer, default = 10)
+    total_pulls = db.Column(db.Integer, default = 0)
+
+class PlayerDragons(db.Model):
+    __tablename__ = 'player_dragons'
+    id = db.Column(db.Integer, primary_key = True)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    dragon_id = db.Column(db.Integer, db.ForeignKey('dragon_stats.id'))
+    lvl = db.Column(db.Integer, default = 1)
+    asc = db.Column(db.Integer, nullable = False)

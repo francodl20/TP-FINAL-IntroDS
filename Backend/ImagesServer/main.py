6c70d8cb968b042bbac0a9db381f7ea9527c5038
Franco de León <45929292+francodl20@users.noreply.github.com>
2024-07-27 21:18:23 -0300

    Images server working

---
+++ Backend/ImagesServer/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+port = 776
+
+#Main Page
+@app.route('/')
+def mainPage():
+    return 'Dragon Gacha API'
+
+#Returns dragon image
+@app.route('/dragons/<id_dragon>')
+def player(id_dragon):
+
+    dragon = 'dragons/' + id_dragon + '.png'
+    
+    return send_file(dragon, mimetype="image/png")
+
+if __name__ == '__main__':
+    print('Starting server...')
+    app.run(host='0.0.0.0', debug=True, port=port)
+    print('Started...')

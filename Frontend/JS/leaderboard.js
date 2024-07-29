3b6c2a023ea53aa7c84e28341c6dcb2f9dbde1ce
Franco de León <45929292+francodl20@users.noreply.github.com>
2024-07-29 14:41:21 -0300

    can go back to menu

--- Frontend/JS/leaderboard.js
+++ Frontend/JS/leaderboard.js
@@ -9,7 +9,7 @@ function fillLeaderboard(content) {
     for(var i = 1; i <= contentLenght; i++) {
         const item = document.createElement("p")
         item.className = "pure-u-1 "
-        item.innerHTML = `<a href="accountMenu.html?playID=${content[i].id}" target="_blank"> ${content[i].name} </a> || Orbes: ${content[i].orbs} - Total de tiradas: ${content[i].totalPulls}`;
+        item.innerHTML = `<a href="accountMenu.html?playID=${content[i].id}"> ${content[i].name} </a> || Orbes: ${content[i].orbs} - Total de tiradas: ${content[i].totalPulls}`;
         container.append(item)
     }
 }
